4f3a50fc77a54710b4b4442d1f1d74a5514b15c0
SputnikPH <zphkindle@163.com>
2018-05-20 15:20:23 +0800

    新增了 KNN中的score方法

--- kNN.py
+++ kNN.py
@@ -3,12 +3,14 @@
 import numpy as np
 from math import sqrt
 from collections import Counter
+from metrics import accuracy_score
 
 
 class KNNClassifier:
 
     def __init__(self, k):
         """初始化kNN分类器"""
+
         assert k >= 1, "k must be valid"
         self.k = k
         self._X_train = None
@@ -16,6 +18,7 @@ class KNNClassifier:
 
     def fit(self, X_train, y_train):
         """根据训练数据集X_train和y_train训练kNN分类器"""
+
         assert X_train.shape[0] == y_train.shape[0], "the size of X_train must be equal to the size of y_train"
         assert self.k <= X_train.shape[0], "the size of X_train must be at least k."
 
@@ -25,6 +28,7 @@ class KNNClassifier:
 
     def predict(self, X_predict):
         """给定待预测数据集X_predict，返回表示X_predict的结果向量"""
+
         assert self._X_train is not None and self._y_train is not None, "must fit before predict!"
         assert X_predict.shape[1] == self._X_train.shape[1], "the feature number of X_predict must be equal to X_train"
 
@@ -33,6 +37,7 @@ class KNNClassifier:
 
     def _predict(self, x):
         """给定单个待预测数据x，返回x的预测结果值"""
+
         assert x.shape[0] == self._X_train.shape[1], "the feature number of x must be equal to X_train"
 
         distances = [sqrt(np.sum((x_train - x) ** 2))
@@ -43,6 +48,12 @@ class KNNClassifier:
         votes = Counter(topK_y)
 
         return votes.most_common(1)[0][0]
+
+    def score(self, X_test, y_test):
+        """根据测试数据集 X_test 和 y_test 确定当前模型的准确度"""
+
+        y_predict = self.predict(X_test)
+        return accuracy_score(y_test, y_predict)
 
     def __repr__(self):
         return "KNN(k=%d)" % self.k
