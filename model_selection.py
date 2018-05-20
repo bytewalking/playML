b0cde0b81d3a949ba49f013256aa1410c5194cc9
SputnikPH <zphkindle@163.com>
2018-05-20 15:10:42 +0800

    美化了代码

--- model_selection.py
+++ model_selection.py
@@ -5,10 +5,8 @@ import numpy as np
 
 def train_test_split(X, y, test_ratio=0.2, seed=None):
     """将数据 X 和 y 按照test_ratio分割成X_train, X_test, y_train, y_test"""
-    assert X.shape[0] == y.shape[0], \
-        "the size of X must be equal to the size of y"
-    assert 0.0 <= test_ratio <= 1.0, \
-        "test_ration must be valid"
+    assert X.shape[0] == y.shape[0], "the size of X must be equal to the size of y"
+    assert 0.0 <= test_ratio <= 1.0, "test_ration must be valid"
 
     if seed:
         np.random.seed(seed)
